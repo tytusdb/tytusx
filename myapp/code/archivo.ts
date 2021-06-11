8b8ebcaabeb7baf1fe802f28c8791a65d8b504db
Jorge Ambrocio Ventura <53419722+JorgeAmbrocio@users.noreply.github.com>
2021-06-11 15:41:19 -0600

    Preparación para publicación en remoto

--- myapp/code/archivo.ts
+++
@@ -1,9 +0,0 @@
-export default class archivo{
-
-    public hola(){
-        console.log("Hola mundo");
-        console.log("nuevoCambio");
-        console.log("nuevo cambio de nuevo");
-    }
-
-}
