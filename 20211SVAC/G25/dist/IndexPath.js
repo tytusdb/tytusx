5e9d2859f9c9ced9a10f54305ac1c60717c69e9a
elder tojin <eldertojins@gmail.com>
2021-06-12 18:44:52 -0600

    [ADD] Agregar gramatica xpath

--- 20211SVAC/G25/dist/IndexPath.js
+++ 20211SVAC/G25/dist/IndexPath.js
@@ -1,10 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Ast_1 = require("./xpath/ast/Ast");
-const gramatica = require('./grammar/Grammar_xpath');
+var gramatica = require('../grammar/Grammar_xpath');
 function ejecutarCodigo(entrada) {
     const instrucciones = gramatica.parse(entrada);
     const AST = new Ast_1.ast(instrucciones);
-    console.log(instrucciones);
+    console.log(AST);
 }
-ejecutarCodigo('/123/fff/asdf');
+ejecutarCodigo("@");
