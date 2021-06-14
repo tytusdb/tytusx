f624c3b4d45c1abe0e5468f30b144c69e15f1a48
201700670 <vgdep1997@gmail.com>
2021-06-14 00:09:03 -0600

    reportes y arbol, tabla simbolos

---
+++ 20211SVAC/G24/Frontend/src/app/Backend/XPATH/Analizador/Simbolos/Tipo.ts
@@ -0,0 +1,26 @@
+export default class Tipo {
+  private tipos: tipoDato;
+  constructor(tipos: tipoDato) {
+    this.tipos = tipos;
+  }
+  public getTipo(): tipoDato {
+    return this.tipos;
+  }
+  public setTipo(tipo: tipoDato): void {
+    this.tipos = tipo;
+  }
+  public igual(compara: Tipo) {
+    return (this.tipos = compara.tipos);
+  }
+}
+
+export enum tipoDato {
+  CADENA,
+  OBJETO,
+  ENTERO,
+  DECIMAL,
+  BOOLEANO,
+  CARACTER,
+  SELECT,
+  ATRIBUTO
+}
