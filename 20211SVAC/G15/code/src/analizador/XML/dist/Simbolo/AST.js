9372a7f97f3a698f94bf7eb31642517722a8ccbd
Horacio-Ciraiz <horacio.ciraiz@gmail.com>
2021-06-15 09:39:29 -0600

    Code

--- 20211SVAC/G15/code/src/analizador/XML/dist/Simbolo/AST.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AST = void 0;
-var AST = /** @class */ (function () {
-    function AST() {
-    }
-    return AST;
-}());
-exports.AST = AST;
