8ac36f527d3e151c659fa32498a901c847484bad
Astrid Martinez <astrid_martinez60@yahoo.es>
2021-06-15 19:49:55 -0600

    modificacion XML-parser

--- 20211SVAC/G22/Scripts/Arboles/Regla.js
+++ 20211SVAC/G22/Scripts/Arboles/Regla.js
@@ -1,31 +1,17 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Tipo_Regla = exports.Regla = void 0;
+exports.Regla = void 0;
 var Regla = /** @class */ (function () {
-    function Regla(tipo, produccion, reglaSemantica) {
-        this.tipo = tipo;
-        this.produccion = produccion;
-        this.reglaSemantica = reglaSemantica;
+    function Regla(tipo1, produccion1, reglaSemantica1) {
+        this.tipo = tipo1;
+        this.produccion = produccion1;
+        this.reglaSemantica = reglaSemantica1;
     }
+    Regla.prototype.getProduccion = function () {
+        return this.produccion;
+    };
+    Regla.prototype.getReglaSemantica = function () {
+        return this.reglaSemantica;
+    };
     return Regla;
 }());
 exports.Regla = Regla;
-var Tipo_Regla;
-(function (Tipo_Regla) {
-    Tipo_Regla[Tipo_Regla["SUMA"] = 0] = "SUMA";
-    Tipo_Regla[Tipo_Regla["RESTA"] = 1] = "RESTA";
-    Tipo_Regla[Tipo_Regla["MULTIPLICACION"] = 2] = "MULTIPLICACION";
-    Tipo_Regla[Tipo_Regla["DIVISION"] = 3] = "DIVISION";
-    Tipo_Regla[Tipo_Regla["MODULO"] = 4] = "MODULO";
-    Tipo_Regla[Tipo_Regla["MENOS_UNARIO"] = 5] = "MENOS_UNARIO";
-    Tipo_Regla[Tipo_Regla["MAYOR_QUE"] = 6] = "MAYOR_QUE";
-    Tipo_Regla[Tipo_Regla["MENOR_QUE"] = 7] = "MENOR_QUE";
-    Tipo_Regla[Tipo_Regla["IGUAL_IGUAL"] = 8] = "IGUAL_IGUAL";
-    Tipo_Regla[Tipo_Regla["DIFERENTE_QUE"] = 9] = "DIFERENTE_QUE";
-    Tipo_Regla[Tipo_Regla["OR"] = 10] = "OR";
-    Tipo_Regla[Tipo_Regla["AND"] = 11] = "AND";
-    Tipo_Regla[Tipo_Regla["NOT"] = 12] = "NOT";
-    Tipo_Regla[Tipo_Regla["MAYOR_IGUA_QUE"] = 13] = "MAYOR_IGUA_QUE";
-    Tipo_Regla[Tipo_Regla["MENOR_IGUA_QUE"] = 14] = "MENOR_IGUA_QUE";
-    Tipo_Regla[Tipo_Regla["DESCONOCIDO"] = 15] = "DESCONOCIDO";
-})(Tipo_Regla = exports.Tipo_Regla || (exports.Tipo_Regla = {}));
