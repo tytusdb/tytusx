a67d5d0199d8b419a48e886fe9e1c988bddcc89e
RaulMendizabal <juanraulmr@hotmail.com>
2021-06-16 00:29:47 -0600

    Se agregaron reportes, correcciones a gramaticas y edición de readme

---
+++ 20211SVAC/G02/Expresiones/Predicados.js
@@ -0,0 +1,135 @@
+"use strict";
+var Operadores;
+(function (Operadores) {
+    Operadores[Operadores["SUMA"] = 0] = "SUMA";
+    Operadores[Operadores["RESTA"] = 1] = "RESTA";
+    Operadores[Operadores["MULTIPLICACION"] = 2] = "MULTIPLICACION";
+    Operadores[Operadores["DIVISION"] = 3] = "DIVISION";
+    Operadores[Operadores["MODULO"] = 4] = "MODULO";
+    Operadores[Operadores["MENOS_UNARIO"] = 5] = "MENOS_UNARIO";
+    Operadores[Operadores["MAYOR_QUE"] = 6] = "MAYOR_QUE";
+    Operadores[Operadores["MENOR_QUE"] = 7] = "MENOR_QUE";
+    Operadores[Operadores["IGUAL_IGUAL"] = 8] = "IGUAL_IGUAL";
+    Operadores[Operadores["DIFERENTE_QUE"] = 9] = "DIFERENTE_QUE";
+    Operadores[Operadores["OR"] = 10] = "OR";
+    Operadores[Operadores["AND"] = 11] = "AND";
+    Operadores[Operadores["NOT"] = 12] = "NOT";
+    Operadores[Operadores["MAYOR_IGUA_QUE"] = 13] = "MAYOR_IGUA_QUE";
+    Operadores[Operadores["MENOR_IGUA_QUE"] = 14] = "MENOR_IGUA_QUE";
+    Operadores[Operadores["DESCONOCIDO"] = 15] = "DESCONOCIDO";
+})(Operadores || (Operadores = {}));
+class Predicados {
+    constructor(op_izquierda, op_derecha, operacion, linea, columna) {
+        this.linea = linea;
+        this.columna = columna;
+        this.op_izquierda = op_izquierda;
+        this.op_derecha = op_derecha;
+        this.operador = operacion;
+    }
+    getTipo(ent, arbol) {
+        const valor = this.getValorImplicito(ent, arbol);
+        if (typeof (valor) === 'boolean') {
+            return Tipo.BOOL;
+        }
+        else if (typeof (valor) === 'string') {
+            return Tipo.STRING;
+        }
+        else if (typeof (valor) === 'number') {
+            if (this.isInt(Number(valor))) {
+                return Tipo.INT;
+            }
+            return Tipo.DOUBLE;
+        }
+        else if (valor === null) {
+            return Tipo.NULL;
+        }
+        return Tipo.VOID;
+    }
+    getValorImplicito(ent, arbol) {
+        if (this.operador !== Operadores.MENOS_UNARIO && this.operador !== Operadores.NOT) {
+            let op1 = this.op_izquierda.getValorImplicito(ent, arbol);
+            let op2 = this.op_derecha.getValorImplicito(ent, arbol);
+            //suma
+            if (this.operador == Operadores.SUMA) {
+                if (typeof (op1 === "number") && typeof (op2 === "number")) {
+                    return op1 + op2;
+                }
+                else if (op1 === "string" || op2 === "string") {
+                    if (op1 == null)
+                        op1 = "null";
+                    if (op2 == null)
+                        op2 = "null";
+                    return op1.ToString() + op2.ToString();
+                }
+                else {
+                    console.log("Error de tipos de datos no permitidos realizando una suma");
+                    return null;
+                }
+            }
+            //resta
+            else if (this.operador == Operadores.RESTA) {
+                if (typeof (op1 === "number") && typeof (op2 === "number")) {
+                    return op1 - op2;
+                }
+                else {
+                    console.log("Error de tipos de datos no permitidos realizando una suma");
+                    return null;
+                }
+            }
+            //multiplicación
+            else if (this.operador == Operadores.MULTIPLICACION) {
+                if (typeof (op1 === "number") && typeof (op2 === "number")) {
+                    return op1 * op2;
+                }
+                else {
+                    console.log("Error de tipos de datos no permitidos realizando una suma");
+                    return null;
+                }
+            }
+            //division
+            else if (this.operador == Operadores.DIVISION) {
+                if (typeof (op1 === "number") && typeof (op2 === "number")) {
+                    if (op2 === 0) {
+                        console.log("Resultado indefinido, no puede ejecutarse operación sobre cero.");
+                        return null;
+                    }
+                    return op1 / op2;
+                }
+                else {
+                    console.log("Error de tipos de datos no permitidos realizando una suma");
+                    return null;
+                }
+            }
+            //modulo
+            else if (this.operador == Operadores.MODULO) {
+                if (typeof (op1 === "number") && typeof (op2 === "number")) {
+                    if (op2 === 0) {
+                        console.log("Resultado indefinido, no puede ejecutarse operación sobre cero.");
+                        return null;
+                    }
+                    return op1 % op2;
+                }
+                else {
+                    console.log("Error de tipos de datos no permitidos realizando una suma");
+                    return null;
+                }
+            }
+        }
+        else {
+            let op1 = this.op_izquierda.getValorImplicito(ent, arbol);
+            if (this.operador == Operadores.MENOS_UNARIO) {
+                if (typeof (op1 === "number")) {
+                    return -1 * op1;
+                }
+                else {
+                    console.log("Error de tipos de datos no permitidos realizando una operación unaria");
+                    return null;
+                }
+            }
+        }
+        return null;
+    }
+    isInt(n) {
+        return Number(n) === n && n % 1 === 0;
+    }
+}
