1d531ffab9e22589b1fa991561adc3ab5f72c59a
Yasmin <yasminmonterrosoe@gmail.com>
2021-06-16 15:11:34 -0600

    [G30]CodigoFuente y otras cosas
    * Análisis Completos
    * Árboles casi completos

---
+++ 20211SVAC/G30/Código/models/tipo.model.ts
@@ -0,0 +1,13 @@
+export enum Tipo {
+  INTEGER = 'integer',
+  STRING = 'string',
+  BOOLEAN = 'boolean',
+  DOUBLE = 'double',
+  VOID = 'void',
+
+  MAIN = 'main',
+  FUNCION = 'funcion',
+  VARIABLE = 'variable',
+  CONSTANTE = 'constante',
+  PRIMITIVO = 'primitivo'
+};
