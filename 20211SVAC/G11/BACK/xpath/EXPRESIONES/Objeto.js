d9dc1eed9b0cdff242d7bf7ed27203f8ce3f1a95
27Pablooliva27 <Pabloolivaa@gmail.com>
2021-06-16 00:27:02 -0600

    Commit xpath

---
+++ 20211SVAC/G11/BACK/xpath/EXPRESIONES/Objeto.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.__esModule = true;
+exports.Objeto = void 0;
+var Objeto = /** @class */ (function () {
+    function Objeto(id, texto, linea, columna, listaO) {
+        this.identificador = id;
+        this.texto = texto;
+        this.linea = linea;
+        this.columna = columna;
+        this.listaObjetos = listaO;
+    }
+    return Objeto;
+}());
+exports.Objeto = Objeto;
