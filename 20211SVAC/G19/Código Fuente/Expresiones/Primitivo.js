f31713fe9eb830ccffb0571c8a541b3868209b8e
carlosngv <carlosngv99@hotmail.com>
2021-06-17 11:55:18 -0600

    FASE2: Estrucrura XQeury

---
+++ 20211SVAC/G19/Código Fuente/Expresiones/Primitivo.js
@@ -0,0 +1,34 @@
+"use strict";
+var Primitivo = /** @class */ (function () {
+    function Primitivo(valor, linea, columna) {
+        this.linea = linea;
+        this.columna = columna;
+        this.valor = valor;
+    }
+    Primitivo.prototype.getTipo = function (ent, arbol) {
+        var valor = this.getValorImplicito(ent, arbol);
+        if (typeof (valor) === 'boolean') {
+            return 'boolean';
+        }
+        else if (typeof (valor) === 'string') {
+            return 'string';
+        }
+        else if (typeof (valor) === 'number') {
+            if (this.isInt(Number(valor))) {
+                return 'number';
+            }
+            return 'double';
+        }
+        else if (valor === null) {
+            return 'null';
+        }
+        return 'void';
+    };
+    Primitivo.prototype.getValorImplicito = function (ent, arbol) {
+        return this.valor;
+    };
+    Primitivo.prototype.isInt = function (n) {
+        return Number(n) === n && n % 1 === 0;
+    };
+    return Primitivo;
+}());
--- 20211SVAC/G19/indexXPath.js
+++ 20211SVAC/G19/indexXPath.js
@@ -1,38 +1,24 @@
 let ast;
-
-i = 0;
-let search;
-
+i=0;
+let  search;
 function AnalizarXpath() {
-
     var ta = document.getElementById(get_vent());
     var contenido = ta.value;
-    contenidoErrores = "";
-    Errores.clear();
-    //try{
+    
     let p = new producion();
-    ast = gramaticaXPath.parse(contenido);
-    if (!Errores.Vacio())
-        console.log("no se encontraron errores")
-    else
-        contenido.Errores = Errores.mostrar_Lista()
-        //  }
-        //  catch(error){
-        //    console.log(error)
-        //  }
-
+        
+       // if (err) throw err;
+        ast =gramaticaXPath.parse(contenido);
+      // RecorrerAst2(ast.tree)
+    console.log(ast.tree)
+    //console.log(ast.tree.children[0]);
+   // i=0;
 }
-
-
-
-function query() {
-    var cadena = search.RecorrerAst(ast.tree);
-    imprimirEnConsola(cadena)
-
+function query(){
+    search.RecorrerAst(ast.tree);
 }
-
-function ASTXPATH() {
+function ASTXPATH(){
     graficar();
-    i = 0;
+    i=0;
 
 }
