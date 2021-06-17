49dbf51ed8b9a47d164ec5d1c3d0b2a061c271ed
Luis Da Castellanos <3006240180101@ingenieria.usac.edu.gt>
2021-06-16 22:27:15 -0600

    Fix predicates G23
    Danniel

--- 20211SVAC/G23/backend/controller/xpath/Instruccion/Selecting/Eje.ts
+++ 20211SVAC/G23/backend/controller/xpath/Instruccion/Selecting/Eje.ts
@@ -4,12 +4,11 @@ import Expresion from "../../Expresion/Expresion";
 import { Element } from "../../../../model/xml/Element";
 import { Atributo } from "../../../../model/xml/Atributo";
 import { Predicate } from "./Predicate";
-import Axis from "./Axis";
+import Axis from "./Axis/Axis";
 
 function Eje(_instruccion: any, _ambito: Ambito, _contexto: any): any {
-    let retorno = { cadena: Tipos.NONE, elementos: null }
     let _404 = { notFound: "No se encontraron elementos." };
-    let contexto: any = (_contexto.elementos) ? (_contexto.elementos) : null;
+    let contexto: any = (_contexto.elementos) ? (_contexto) : null;
     let expresion;
     if (_instruccion.expresion.expresion) expresion = Expresion(_instruccion.expresion.expresion, _ambito, contexto);
     else expresion = Expresion(_instruccion.expresion, _ambito, contexto);
@@ -21,34 +20,32 @@ function Eje(_instruccion: any, _ambito: Ambito, _contexto: any): any {
     }
     else if (expresion.tipo === Tipos.ATRIBUTOS) {
         root = getSymbolFromRoot({ id: expresion.valor, tipo: "@" }, contexto, _ambito, predicate);
-        if (root.atributos.length === 0) return _404;
         if (root.atributos.error) return root.atributos;
+        if (root.atributos.length === 0) return _404;
+        return root;
     }
     else if (expresion.tipo === Tipos.ASTERISCO) {
         root = getSymbolFromRoot(expresion.valor, contexto, _ambito, predicate);
     }
     else if (expresion.tipo === Tipos.FUNCION_NODE) {
         root = getSymbolFromRoot(expresion.valor, contexto, _ambito, predicate);
-        if (root.nodos.length === 0) return _404;
         if (root.nodos.error) return root.nodos;
+        if (root.nodos.length === 0) return _404;
     }
     else if (expresion.tipo === Tipos.FUNCION_TEXT) {
         root = getSymbolFromRoot(expresion.valor, contexto, _ambito, predicate);
-        if (root.texto.length === 0) return _404;
         if (root.texto.error) return root.texto;
+        if (root.texto.length === 0) return _404;
     }
     else if (expresion.tipo === Tipos.SELECT_AXIS) {
-        root = Axis.GetAxis(expresion.axisname, expresion.nodetest, expresion.predicate, contexto, _ambito);
-        if (root.error) return root;
-        if (root.atributos.error) return root.atributos;
+        root = Axis.GetAxis(expresion.axisname, expresion.nodetest, expresion.predicate, contexto, _ambito, false);
+        return root;
     }
     else {
         return { error: "Expresión no válida.", tipo: "Semántico", origen: "Query", linea: _instruccion.linea, columna: _instruccion.columna };
     }
-    if (root === null || root.error || root.elementos.length === 0) return _404;
-    if (root.elementos.error) return root.elementos;
-    retorno = root;
-    return retorno;
+    if (root === null || root.error || root.elementos.error || root.elementos.length === 0) return _404;
+    return root;
 }
 
 function getSymbolFromRoot(_nodename: any, _contexto: Array<Element>, _ambito: Ambito, _condicion: any): any {
@@ -65,8 +62,8 @@ function getFromCurrent(_id: any, _contexto: any, _ambito: Ambito, _condicion: a
     // Selecciona el texto contenido únicamente en el nodo
     if (_id === "text()") {
         let text = Array<string>();
-        for (let i = 0; i < _contexto.length; i++) {
-            const element = _contexto[i];
+        for (let i = 0; i < _contexto.elementos.length; i++) {
+            const element = _contexto.elementos[i];
             if (element.value) {
                 text.push(element.value);
                 elements.push(element);
@@ -82,8 +79,8 @@ function getFromCurrent(_id: any, _contexto: any, _ambito: Ambito, _condicion: a
     // Selecciona todos los hijos (elementos o texto)
     else if (_id === "node()") {
         let nodes = Array<any>();
-        for (let i = 0; i < _contexto.length; i++) {
-            const element = _contexto[i];
+        for (let i = 0; i < _contexto.elementos.length; i++) {
+            const element = _contexto.elementos[i];
             if (element.childs)
                 element.childs.forEach((child: Element) => {
                     nodes.push({ elementos: child });
@@ -94,13 +91,14 @@ function getFromCurrent(_id: any, _contexto: any, _ambito: Ambito, _condicion: a
         if (_condicion) {
             let filter = new Predicate(_condicion, _ambito, elements);
             nodes = filter.filterElements(nodes);
+            elements = filter.contexto;
         }
-        return { cadena: Tipos.COMBINADO, nodos: nodes, elementos: _contexto };
+        return { cadena: Tipos.COMBINADO, nodos: nodes, elementos: _contexto.elementos };
     }
     // Selecciona todos los hijos (elementos)
     else if (_id === "*") {
-        for (let i = 0; i < _contexto.length; i++) {
-            const element = _contexto[i];
+        for (let i = 0; i < _contexto.elementos.length; i++) {
+            const element = _contexto.elementos[i];
             if (element.childs) {
                 element.childs.forEach((child: Element) => {
                     elements.push(child);
@@ -115,30 +113,23 @@ function getFromCurrent(_id: any, _contexto: any, _ambito: Ambito, _condicion: a
     }
     // Selecciona los atributos
     else if (_id.tipo === "@") {
-        let flag: boolean = false;
-        for (let i = 0; i < _contexto.length; i++) {
-            const element = _contexto[i];
+        for (let i = 0; i < _contexto.elementos.length; i++) {
+            const element = _contexto.elementos[i];
             if (element.attributes)
                 element.attributes.forEach((attribute: Atributo) => {
-                    if ((_id.id === attribute.id) || (_id.id === "*")) { // En caso de que sea un id ó @*
+                    if ((_id.id == attribute.id) || (_id.id === "*")) { // En caso de que sea un id ó @*
                         attributes.push(attribute);
-                        flag = true;
                     }
                 });
-            if (flag) {
-                elements.push(element);
-                flag = false;
-            }
         }
         if (_condicion) {
             let filter = new Predicate(_condicion, _ambito, elements);
-            attributes = filter.filterElements(attributes);
-            elements = filter.contexto;
+            attributes = filter.filterAttributes(attributes);
         }
-        return { atributos: attributes, elementos: elements, cadena: Tipos.ATRIBUTOS };
+        return { atributos: attributes, elementos: [], cadena: Tipos.ATRIBUTOS };
     }
     // Selecciona el padre
-    else if (_id === "..") {
+    else if (_id === "..") { // Manejar el regreso de atributos a su padre como la etiqueta misma !
         if (_contexto.atributos) {
             for (let i = 0; i < _contexto.atributos.length; i++) {
                 const attribute = _contexto.atributos[i];
@@ -146,10 +137,14 @@ function getFromCurrent(_id: any, _contexto: any, _ambito: Ambito, _condicion: a
                     elements = _ambito.searchDadFromAttribute(elm, attribute, elements);
                 });
             }
-            return { elementos: elements };
+            if (_condicion) {
+                let filter = new Predicate(_condicion, _ambito, elements);
+                elements = filter.filterElements(elements);
+            }
+            return { elementos: elements, cadena: Tipos.ELEMENTOS };
         }
-        for (let i = 0; i < _contexto.length; i++) {
-            const element = _contexto[i];
+        for (let i = 0; i < _contexto.elementos.length; i++) {
+            const element = _contexto.elementos[i];
             let dad = element.father;
             if (dad) {
                 _ambito.tablaSimbolos.forEach(elm => {
@@ -170,8 +165,11 @@ function getFromCurrent(_id: any, _contexto: any, _ambito: Ambito, _condicion: a
     }
     // Selecciona el nodo actual
     else if (_id === ".") {
-        for (let i = 0; i < _contexto.length; i++) {
-            const element = _contexto[i];
+        if (_contexto.atributos) {
+            return { elementos: [], atributos: _contexto.atributos, cadena: Tipos.ATRIBUTOS };
+        }
+        for (let i = 0; i < _contexto.elementos.length; i++) {
+            const element = _contexto.elementos[i];
             elements.push(element);
         }
         if (_condicion) {
@@ -182,8 +180,8 @@ function getFromCurrent(_id: any, _contexto: any, _ambito: Ambito, _condicion: a
     }
     // Búsqueda en los hijos por id
     else {
-        for (let i = 0; i < _contexto.length; i++) {
-            const element = _contexto[i];
+        for (let i = 0; i < _contexto.elementos.length; i++) {
+            const element = _contexto.elementos[i];
             if (element.childs) {
                 element.childs.forEach((child: Element) => {
                     elements = _ambito.searchSingleNode(_id, child, elements);
@@ -191,10 +189,8 @@ function getFromCurrent(_id: any, _contexto: any, _ambito: Ambito, _condicion: a
             }
         }
         if (_condicion) {
-            console.log(elements, 7777777)
             let filter = new Predicate(_condicion, _ambito, elements);
             elements = filter.filterElements(elements);
-            console.log(elements, 888888)
         }
         return { elementos: elements, cadena: Tipos.ELEMENTOS };
     }
--- 20211SVAC/G23/backend/model/xml/Encoding/Enum.ts
+++ 20211SVAC/G23/backend/model/xml/Encoding/Enum.ts
@@ -1,7 +1,7 @@
 
 export enum Codes {
-    UTF8,
-    ASCII,
-    ISO8859_1,
-    INVALID
+    UTF8 = "utf-8",
+    ASCII = "ascii",
+    ISO8859_1 = "latin1",
+    INVALID = "invalid"
 }
