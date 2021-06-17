7459d72f0d2059bd22935104fc905a9736ffa0d9
Alejandro Marín <3019371840101@Ingenieria.usac.edu.gt>
2021-06-16 22:56:44 -0600

    codigo fuente

---
+++ 20211SVAC/G15/code/src/analizador/XML/dist/Expresiones/Primitivo.js
@@ -0,0 +1,38 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Primitivo = void 0;
+var Tipo_1 = require("../AST/Tipo");
+var Primitivo = /** @class */ (function () {
+    function Primitivo(valor, linea, columna) {
+        this.linea = linea;
+        this.columna = columna;
+        this.valor = valor;
+    }
+    Primitivo.prototype.getTipo = function (ent, arbol) {
+        var valor = this.getValorImplicito(ent, arbol);
+        if (typeof (valor) === 'boolean') {
+            return Tipo_1.Tipo.BOOL;
+        }
+        else if (typeof (valor) === 'string') {
+            return Tipo_1.Tipo.STRING;
+        }
+        else if (typeof (valor) === 'number') {
+            if (this.isInt(Number(valor))) {
+                return Tipo_1.Tipo.INT;
+            }
+            return Tipo_1.Tipo.DOUBLE;
+        }
+        else if (valor === null) {
+            return Tipo_1.Tipo.NULL;
+        }
+        return Tipo_1.Tipo.VOID;
+    };
+    Primitivo.prototype.getValorImplicito = function (ent, arbol) {
+        return this.valor;
+    };
+    Primitivo.prototype.isInt = function (n) {
+        return Number(n) === n && n % 1 === 0;
+    };
+    return Primitivo;
+}());
+exports.Primitivo = Primitivo;
