5e93545e661c2ae508647cb1f8ecc1530762d0a6
SergioG-SG <sergio.g.usac@gmail.com>
2021-06-17 11:38:20 -0600

    cambios en la búsqueda del XPath
    Co-Authored-By: Julio-usac <69477547+Julio-usac@users.noreply.github.com>

---
+++ 20211SVAC/G05/build/Interprete/Expresion/Sacceso.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Sacceso = void 0;
+class Sacceso {
+    constructor(tipoinicio, Nacceso, linea, columna) {
+        this.tipoinicio = tipoinicio;
+        this.Nacceso = Nacceso;
+        this.linea = linea;
+        this.columna = columna;
+    }
+}
+exports.Sacceso = Sacceso;
