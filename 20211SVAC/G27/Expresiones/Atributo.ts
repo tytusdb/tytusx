d9d56ebd18198d55df1eb9297dba6929ad99bb1d
alwuud <dahh.drag@gmail.com>
2021-06-19 13:50:18 -0600

    Se agregaron y corrigieron archivos TS

--- 20211SVAC/G27/Expresiones/Atributo.ts
+++ 20211SVAC/G27/Expresiones/Atributo.ts
@@ -3,13 +3,11 @@ class Atributo{
     valor:string;
     linea:number;
     columna:number;
-    tipo: string;
 
     constructor(id:string, valor:string, linea:number, columna:number){
         this.identificador = id;
         this.valor = valor;
         this.linea = linea;
         this.columna = columna;
-        this.tipo = 'Atributo';
     }
 }
