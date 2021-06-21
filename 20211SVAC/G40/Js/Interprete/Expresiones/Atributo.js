15a9c392ed96f2e77b1f6121389f227c8bd46346
Oscar Llamas <oscarllamasusac@gmail.com>
2021-06-20 19:35:22 -0600

    [Fase 2] Traducción de XML a C3D, contenido en el heap y referencias en el stack

--- 20211SVAC/G40/Js/Interprete/Expresiones/Atributo.js
+++ 20211SVAC/G40/Js/Interprete/Expresiones/Atributo.js
@@ -6,7 +6,18 @@ var Atributo = /** @class */ (function () {
         this.valor = valor;
         this.linea = linea;
         this.columna = columna;
+        this.posicion = -1;
     }
+
+    Atributo.prototype.SetearPosicion = function (pos) {
+        if(this.posicion==-1){
+            this.posicion = pos;
+        }    
+    };
+
+    Atributo.prototype.getPosicion = function () {
+        return this.posicion;
+    };   
 
     Atributo.prototype.getID = function () {
         return this.identificador;
