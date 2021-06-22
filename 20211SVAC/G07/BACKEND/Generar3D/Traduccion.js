1f42ec16c789b60d715f62136903537bdfcbe2dd
Aybson Mercado <aybsonmg@gmail.com>
2021-06-22 01:13:00 -0600

    Se agregan metodos para generar codigo de 3Direcciones a partir de tipos numericos y se genera el metodo para obtener dicho dato del heap a partir de la referencia

--- 20211SVAC/G07/BACKEND/Generar3D/Traduccion.js
+++ 20211SVAC/G07/BACKEND/Generar3D/Traduccion.js
@@ -11,6 +11,37 @@ class Traduccion {
 
     }
 
+    //Metodo que permite traducir un numero a codigo de 3Direcciones y lo agrega al Heap
+    traducirNumero(numero) {
+        //Se obtiene la posicion del heap
+        this.t0 = this.h;
+        this.traduccion3D += `
+            t${this.t} = H;`;
+            
+        //Temporal que manejara la asignacion al heap
+        this.t1 = this.t0;
+        this.traduccion3D += `
+            t${this.t+1} = t${this.t};`;
+
+        //Aumento del uso del heap
+        this.h = this.h + 1;
+        this.traduccion3D += `
+            H = H + 1;`;
+
+        //Asignar al heap el numero
+        this.heap[this.t1] = numero;
+        this.traduccion3D += `
+            heap[(int) t${this.t+1}] = ${numero};
+            `;
+
+        //Aumentando el valor de los temporales
+        this.t += 2;
+
+
+        //Retornando la direccion o posicion donde se empieza el almacenamiento
+        return this.t0;
+    }
+
     // Metodo que permite traducir una cadena a codigo de 3Direcciones y lo agrega al Heap
     traducirCadena(cadena) {        
         //Se obtiene la posicion del heap
@@ -55,13 +86,25 @@ class Traduccion {
 
     }
 
+    //Metodo para obtener del Heap un Numero
+    obtenerNumero(indice) {
+        //Valor numerico obtenido del heap
+        return this.heap[indice];
+    }
+
     //Metodo para obtener del Heap un String
     obtenerCadena(indice) {
+        //Cadena que se va a generar
         let cadena = '';
+        //Indice que va a manejar la referencia para obtener los caracteres
         let cont = indice;
         
+        //Ciclo para recorrer los caracteres 
         while (true) {
+            // Se obtiene el caracter
             let asciiChar = this.heap[cont];
+
+            //Se verifica si el caracteres es de fin de cadena
             if (asciiChar !== -1) {
                 cadena += String.fromCharCode(asciiChar);
             } else {
@@ -71,7 +114,7 @@ class Traduccion {
             cont++;
         }
 
-        console.log("Cadena Salida", cadena);
+        return cadena;
     }
 
     obtenerCodigo() {
--- 20211SVAC/G07/BACKEND/Generar3D/pruebaTraduccion.js
+++ 20211SVAC/G07/BACKEND/Generar3D/pruebaTraduccion.js
@@ -1,10 +1,20 @@
 let traductor = new Traduccion();
 
 
-let ref1 = traductor.traducirCadena("hola 123 2");
-let ref2 = traductor.traducirCadena("Cadena qwe @");
+//let ref1 = traductor.traducirCadena("hola");
+//let ref2 = traductor.traducirCadena("Cadena");
 //console.log(traductor.obtenerCodigo());
 
-traductor.obtenerCadena(ref1);
-console.log("-----");
-traductor.obtenerCadena(ref2);
+//traductor.obtenerCadena(ref1);
+//console.log("-----");
+//traductor.obtenerCadena(ref2);
+
+let ref1 = traductor.traducirNumero(65);
+let ref2 = traductor.traducirCadena("Ahjk");
+let ref3 = traductor.traducirNumero(12.4);
+let ref4 = traductor.traducirCadena("Cadena");
+let ref5 = traductor.traducirNumero(30);
+
+
+console.log(traductor.obtenerCodigo());
+
