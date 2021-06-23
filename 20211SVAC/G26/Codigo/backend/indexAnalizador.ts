b0a53085c737558746cae17239bd8cde77d24d09
201503702 <201503702@ingenieria.usac.edu.gt>
2021-06-22 22:25:50 -0600

    Arreglos Reportes y Manuales
    Correccion de reportes, modificacion de manuales y reporte gramatical.

--- 20211SVAC/G26/Codigo/backend/indexAnalizador.ts
+++ 20211SVAC/G26/Codigo/backend/indexAnalizador.ts
@@ -31,7 +31,7 @@ class Analizador{
     Analizador._instance = this;
     return this;
   }
-  
+
   public static getInstance() {
     return this._instance;
   }
@@ -155,7 +155,7 @@ class Analizador{
                 +            '<td>'+this.getTipoDato(elem.valor.tipo)+'</td>'
                 +            '<td>'+entrada.nombre+'</td>'
                 +            '<td>'+elem.nombre+'</td>'
-                +            '<td>'+elem.valor.valor.toString().replace('&','and')+'</td>'
+                +            '<td>Nodo</td>'
                 +            '<td>'+elem.valor.linea+'</td>'
                 +            '<td>'+elem.valor.columna+'</td>'
                 +        '</tr>';
@@ -197,14 +197,10 @@ class Analizador{
   getRepErrores():string{
     let cadenaDot:string = '';
     let indice:number = 0;
-    cadenaDot = 'digraph {'
-                +  'tbl ['
-                +    'shape=plaintext,'
-                +    'label=<'
-                +      '<table border="0" cellborder="1" color="blue" cellspacing="0">'
+    cadenaDot = '<table class="tablaDatos" >'
                 +        '<tr>'
-                +            '<td>No.</td><td>Tipo</td><td>Descripcion</td><td>Linea</td><td>Columna</td>'
-                +        '</tr>';
+                +            '<th>No.</th><th>Tipo</th><th>Descripcion</th><th>Linea</th><th>Columna</th>'
+                +        '</th>';
     errores.listaError.forEach((elem:mierror) => {
       indice++;
       cadenaDot = cadenaDot
@@ -216,9 +212,7 @@ class Analizador{
                 +            '<td>'+elem.getColumna()+'</td>'
                 +        '</tr>';
     });
-    cadenaDot = cadenaDot +      '</table>'
-                          +    '>];'
-                          +'}';
+    cadenaDot = cadenaDot +      '</table>';
 
     return cadenaDot;
   }
@@ -276,27 +270,9 @@ bookstore/book
 |
 //@category
 `);
+*/
 
-xmlDescendente(`
-<?xml version="1.0" encoding="UTF-8"?>
-
-<bookstore>
-  <book category="children">
-  	<title>Harry Potter</title>
-    <author>J K. Rowlin</author>
-    <price at="asd"></price>
-    <hola> </Hola>
-  </book>
-  <!-- HOLAAA -->
-  <book category="web">
-    <title>Learning XML</title>
-    <author>Erik T. Ray</author>
-    <year>2003</year>
-    <price>39.95 &lt 30</price>
-  </book>
-</bookstore>
-`);
-
+/*
 xmlAscendente(`
 <?xml version="1.0" encoding="UTF-8"?>
 
