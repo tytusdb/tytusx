80c84c583cd44d11c73070e48021fe3360023db9
jaimeisb <jaimeisb2@gmail.com>
2021-06-30 06:17:39 +0200

    Cambios antes de iniciar funciones letales

---
+++ 20211SVAC/G08/CodigoFuente/src/app/Estructuras/Entrada.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+exports.Entrada = void 0;
+var Entrada = /** @class */ (function () {
+    function Entrada(xQueryData, xPathData, Tipo) {
+        this.xQueryData = xQueryData;
+        this.xPathData = xPathData;
+        this.Tipo = Tipo;
+    }
+    return Entrada;
+}());
+exports.Entrada = Entrada;
