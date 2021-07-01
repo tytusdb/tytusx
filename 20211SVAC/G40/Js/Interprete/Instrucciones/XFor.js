cdd78e39bdec0f85e00d1541467e2ccd355133f9
Oscar Llamas <oscarllamasusac@gmail.com>
2021-07-01 11:18:11 -0600

    [XQuery] Funciones Ackerman, Fibonacci & Factorial

---
+++ 20211SVAC/G40/Js/Interprete/Instrucciones/XFor.js
@@ -0,0 +1,54 @@
+"use strict";
+
+var XFor = /** @class */ (function () {
+    function XFor(linea, columna, min, max, etiqueta1, etiqueta2, id1, id2) {
+        this.linea = linea;
+        this.columna = columna;
+        this.min = min;
+        this.max = max;
+        this.etiqueta1 = etiqueta1;
+        this.etiqueta2 = etiqueta2;
+        this.id1 = id1;
+        this.id2 = id2;
+        this.tipo = TipoXInstruccion.XFOR;
+    }
+    XFor.prototype.ejecutar = function (ent, arbol) {
+
+        
+        var expAux = "";
+        if(this.etiqueta1 == this.etiqueta2){
+
+            if(this.min <= this.max){
+
+                if(this.id1 == this.id2){
+
+                    for (var i = this.min-1; i < this.max; i++) {
+
+                        expAux += `<${this.etiqueta1}>${i+1}</${this.etiqueta2}>\n`;
+                        
+                      }
+
+                      return expAux;
+
+                } else{
+                    ListaErr.agregarError(new Error(NumeroE, this.linea, this.columna, "Semántico", this.id2 + " no ha sido declarado","XQUERY")); NumeroE++;
+                    return expAux;
+                }
+
+            } else {
+                ListaErr.agregarError(new Error(NumeroE, this.linea, this.columna, "Semántico", "El primer valor númerico debe ser menor  o igual que el segundo","XQUERY")); NumeroE++;
+                return expAux;
+            }
+
+        }else {
+            ListaErr.agregarError(new Error(NumeroE, this.linea, this.columna, "Semántico", this.etiqueta1 +` no es igual que `+this.etiqueta2,"XQUERY")); NumeroE++;
+            return expAux;
+        }
+
+    };
+
+    XFor.prototype.getTipo = function (){
+        return this.tipo;
+    };
+    return XFor;
+}());
