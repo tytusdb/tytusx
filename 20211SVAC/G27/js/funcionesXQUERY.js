6310386f34c8c3fdcc36be15e8af0d63f6e52c92
Luiskr1993 <luiskr1993@gmail.com>
2021-07-04 01:18:17 -0600

    G27 gramatica XQUERY, modificaciones varias

--- 20211SVAC/G27/js/funcionesXQUERY.js
+++ 20211SVAC/G27/js/funcionesXQUERY.js
@@ -1,3 +1,29 @@
+/* FUNCIONES PARA EJECUCIÓN DE XQUERY */
+const parseXQUERY = function(entrada){
+    var mensajeConsola = "";
+    try {
+        console.log("***************************************************\n");
+        console.log("**************ANALISIS DE XQUERY*******************\n");
+        document.getElementById('consola').innerHTML += "\n>**********************************************\n"
+                                                        + ">**************ANALISIS DE XQUERY***********\n";
+        try {
+            let resultado = gramaticaXQUERY.parse(entrada);
+            if (resultado != undefined){
+                console.log(">Se ejecutó correctamente el parser de XQUERY.\n");
+                document.getElementById('consola').innerHTML += ">Se ejecutó correctamente el parser de XQUERY. \n";
+            }else{
+                console.log(">Undefinde Se ejecutó correctamente el parser de XQUERY.\n");
+                document.getElementById('consola').innerHTML += ">Undefinded Se ejecutó correctamente el parser de XQUERY. \n";
+            }
+        } catch (e) {
+            console.log(">[ERROR]:Ocurrió un error en el parseo del contenido XQUERY\n");
+                document.getElementById('consola').innerHTML += ">[ERROR]:Ocurrió un error en el parseo del contenido XQUERY \n";
+        }
+    } catch (error) {
+        document.getElementById('consola').innerHTML += ">" + error.toString(); 
+    }
+}
+
 /* codemirror para textarea de XQUERY */
 var code2 = document.getElementById("EntradaXQuery");
 var editor2 = CodeMirror.fromTextArea(code2, {
