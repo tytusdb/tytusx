307cf3d843465e43c8cfd4c28fc7caaf3ac4c3f0
Milton <3018849780101@ingenieria.usac.edu.gt>
2021-07-04 22:35:39 -0600

    primitivo + select

--- 20211SVAC/G29/src/clases/expresiones/select.js
+++ 20211SVAC/G29/src/clases/expresiones/select.js
@@ -33,6 +33,9 @@ export default class select {
         else if (this.tipe == "//" && this.id == null && this.atr == true) {
             this.lookAllUnknownP(ent, arbol);
         }
+        else if (this.tipe == "/" && this.id == null && this.atr == true) {
+            this.lookAtUnknownP(ent, arbol);
+        }
         else {
             console.log("NO MATCH");
         }
@@ -247,6 +250,144 @@ export default class select {
                 }
             }
         }
+    }
+    traducir(ent, c3d) {
+        c3d.main += `\t/* ini select */\n`;
+        //posiciones parametros tipe
+        let ret = { "id": c3d.generateTemp(), "val": c3d.s + c3d.last_stack };
+        c3d.main += `\tt${ret.id} = S + ${c3d.last_stack};\t\t//posicion return\n`;
+        let pos = { "id": c3d.generateTemp(), "val": ret.val + 1 };
+        c3d.main += `\tt${pos.id} = t${ret.id} + 1;\t\t//El ultimo stack disponible\n`;
+        //ini cadena
+        c3d.main += `\t// se añade la cadena ${this.id}\n`;
+        let ini = { "id": c3d.generateTemp(), "val": c3d.h };
+        c3d.main += `\tt${ini.id} = H;\n`;
+        for (let i = 0; i < this.id.length; i++) { //se guarda caracter por caracter
+            c3d.heap[c3d.h] = this.id.charCodeAt(i);
+            c3d.main += `\theap[(int)H] = ${this.id.charCodeAt(i)};\t\t//se agrega el caracter H[${c3d.h}] ${this.id.charAt(i)}\n`;
+            c3d.h += 1;
+            c3d.main += `\tH = H + 1;\n`;
+        }
+        c3d.heap[c3d.h] = -1; //se guarda el fin de la cadena
+        c3d.main += `\theap[(int)H] = -1;\t\t//se agrega el caracter eos H[${c3d.h}] -1\n`;
+        c3d.h += 1;
+        c3d.main += `\tH = H + 1;\n`;
+        //se guarda la referencia al heap en el stack
+        c3d.stack[pos.val] = ini.val;
+        c3d.main += `\tstack[(int)t${pos.id}] = t${ini.id};\t\t//se guarda la referencia al heap en el stack: stack[${pos.val}] = ${ini.val}\n`;
+        c3d.temp[ini.id] = ini.val;
+        c3d.temp[pos.id] = pos.val;
+        c3d.temp[ret.id] = ret.val;
+        //end cadena
+        //c3d.addStr(this.id, pos.val)
+        c3d.stack[ret.val] = c3d.h;
+        c3d.main += `\tstack[(int)t${ret.id}] = H;\t\t//posicion del retorno stack[${ret.val}]\n`;
+        //traduccion select
+        if (this.atr == false) { //     /nombre | /*
+            this.lookNodes3D(ent, c3d, ret.id);
+        }
+        else if (this.atr == true) { //     /@atr | /@*
+            this.lookParams3D(ent, c3d, ret.id);
+        }
+        else {
+            console.log("NO MATCH");
+        }
+        c3d.main += `\t/* fin select */\n`;
+        c3d.t_res = ret.id;
+    }
+    lookNodes3D(ent, c3d, pos_param) {
+        c3d.main += `\t/* ini look nodes */\n`;
+        for (let n_ent of ent) {
+            let simbol = n_ent.tabla["id"];
+            let ret = { "id": pos_param, "val": c3d.temp[pos_param] };
+            //la siguiente posicion disponible id xml
+            let pos = { "id": c3d.generateTemp(), "val": ret.val + 2 };
+            c3d.main += `\tt${pos.id} = t${ret.id} + 2;\t\t//La siguiente posicion id xml\n`;
+            //se guarda la posicion (heap) del id
+            c3d.stack[pos.val] = simbol.stack + 1;
+            c3d.main += `\tstack[(int)t${pos.id}] = ${simbol.stack} + 1;\t\t//guarda stack del id xml: stack[${pos.val}] = ${simbol.stack} + 1\n`;
+            c3d.temp[pos.id] = pos.val;
+            //se cambia de entorno
+            c3d.s = c3d.s + c3d.last_stack;
+            c3d.main += `\tS = S + ${c3d.last_stack};\t\t//Establece posicion return\n`;
+            //llamada()
+            c3d.main += `\tmatchId();\n`;
+            c3d.s = c3d.s - c3d.last_stack;
+            c3d.main += `\tS = S - ${c3d.last_stack};\t\t//Establece posicion return\n`;
+            if (this.id == "*") {
+                //SE GUARDA EL VALOR
+                c3d.heap[c3d.h] = simbol.stack;
+                c3d.h += 1;
+                //console.log("MATCH " + c3d.heap[c3d.h])
+            }
+            if (simbol.valor == this.id) {
+                //SE GUARDA EL VALOR
+                c3d.heap[c3d.h] = simbol.stack;
+                c3d.h += 1;
+                //console.log("MATCH " + c3d.heap[c3d.h])
+            }
+        }
+        c3d.heap[c3d.h] = -1;
+        c3d.h += 1;
+        c3d.main += `\theap[(int)H] = -1;\t\t//fin del select\n`;
+        c3d.main += `\tH = H + 1;\t\t//siguiente espacio en heap\n`;
+        c3d.last_stack += 3;
+        c3d.main += `\t/* fin look nodes */\n`;
+    }
+    lookParams3D(ent, c3d, pos_param) {
+        c3d.main += `\t/* ini look params */\n`;
+        for (let n_ent of ent) {
+            let id = n_ent.tabla["id"];
+            for (let key in n_ent.tabla) {
+                if (key.startsWith("atr")) {
+                    //retorno
+                    let ret = { "id": pos_param, "val": c3d.temp[pos_param] };
+                    //seleccion del atributo
+                    let simbol = n_ent.tabla[key];
+                    let pos = { "id": c3d.generateTemp(), "val": ret.val + 2 };
+                    //la siguiente posicion disponible param xml
+                    c3d.main += `\tt${pos.id} = t${ret.id} + 2;\t\t//La siguiente posicion param xml\n`;
+                    //se guarda la posicion (heap) del param
+                    c3d.stack[pos.val] = simbol.stack;
+                    c3d.main += `\tstack[(int)t${pos.id}] = ${simbol.stack};\t\t//guarda stack del param xml: stack[${pos.val}] = ${simbol.stack} \n`;
+                    //la siguiente posicion disponible id xml
+                    //pos.val = pos.val + 1
+                    pos.val += 1;
+                    c3d.main += `\tt${pos.id} = t${pos.id} + 3;\t\t//La siguiente posicion id xml\n`;
+                    //se guarda la posicion (heap) del param
+                    c3d.stack[pos.val] = simbol.stack;
+                    c3d.main += `\tstack[(int)t${pos.id}] = ${id.stack};\t\t//guarda stack del id xml: stack[${pos.val}] = ${id.stack} \n`;
+                    c3d.temp[pos.id] = pos.val;
+                    //se cambia de entorno
+                    c3d.s = c3d.s + c3d.last_stack;
+                    c3d.main += `\tS = S + ${c3d.last_stack};\t\t//Establece posicion return\n`;
+                    //llamada()
+                    c3d.main += `\tmatchAtr();\n`;
+                    c3d.s = c3d.s - c3d.last_stack;
+                    c3d.main += `\tS = S - ${c3d.last_stack};\t\t//Establece posicion return\n`;
+                    if (this.id == "*") {
+                        //SE GUARDA EL VALOR
+                        c3d.heap[c3d.h] = simbol.stack;
+                        c3d.h += 1;
+                        break;
+                        //console.log("MATCH " + c3d.heap[c3d.h])
+                    }
+                    if (simbol.id == this.id) {
+                        //SE GUARDA EL VALOR
+                        c3d.heap[c3d.h] = simbol.stack;
+                        c3d.h += 1;
+                        break;
+                        //console.log("MATCH " + c3d.heap[c3d.h])
+                    }
+                }
+            }
+        }
+        c3d.heap[c3d.h] = -1;
+        c3d.h += 1;
+        c3d.main += `\theap[(int)H] = -1;\t\t//fin del select\n`;
+        c3d.main += `\tH = H + 1;\t\t//siguiente espacio en heap\n`;
+        c3d.last_stack += 4;
+        c3d.main += `\t/* fin look params */\n`;
     }
 }
 //# sourceMappingURL=select.js.map
